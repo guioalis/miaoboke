0dc923c6bcc481c72f38991a4b7d6f1d16256a58
Miaoge <8201072@gmail.com>
2025-04-02 16:27:14 +0800

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import deno from '@astrojs/deno'; // 添加这一行
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), react()],
+    output: 'server', // 修改为server输出模式
+    adapter: deno(), // 添加Deno适配器
 });
